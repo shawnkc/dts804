f12a82fbd708f67e5259dba27c68e6a4fc79d96d
shawnkc <shawn.casey@gmail.com>
2022-07-25 10:45:48 -0500

    Initial Commit

---
+++ dts804/dts804App.swift
@@ -0,0 +1,17 @@
+//
+//  dts804App.swift
+//  dts804
+//
+//  Created by Shawn Casey on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct dts804App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
